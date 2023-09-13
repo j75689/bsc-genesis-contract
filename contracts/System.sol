472b176f49ec31abc7891c3abe178aff9a821d39
j75689 <j75689@gmail.com>
2023-09-13 17:27:58 +0800

    feat: implement merkle Distributor contract

--- contracts/System.sol
+++ contracts/System.sol
@@ -33,6 +33,7 @@ contract System {
   address public constant STAKING_CONTRACT_ADDR = 0x0000000000000000000000000000000000002001;
   address public constant STAKE_HUB_ADDR = 0x0000000000000000000000000000000000002002;
   address public constant GOVERNOR_ADDR = 0x0000000000000000000000000000000000002004;
+  address public constant MERKLE_DISTRIBUTOR_CONTRACT_ADDR = 0x0000000000000000000000000000000000003000;
 
   modifier onlyCoinbase() {
     require(msg.sender == block.coinbase, "the message sender must be the block producer");
